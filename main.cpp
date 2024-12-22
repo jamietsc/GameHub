a4371fd4a8223c3b9d9deb5ae39abfbbac7f790e
jamietsc <jamie.jentsch2007@gmail.com>
2024-12-22 13:38:14 +0100

    Put checking the user input in an extra function.

--- main.cpp
+++ main.cpp
@@ -2,23 +2,19 @@
 #include <cstdlib>
 
 bool checkNumber(int input, int x);
+int checkUserInput();
 
 using namespace std;
 
 int main() {
     int x = rand() % 100 + 1; //x is the random number between 1 and 100
-    bool end;// variable to end the loop
+    bool end = false;// variable to end the loop
     int input, trys = 0; //input is for saving the guess of the user. trys for counting the trys the user needed.
 
-    cout << "The number is set, you can write your answer now." << endl;
-    do{
-        cin >> input;
-        if(cin.fail()){
-            cin.clear(); //reset the error
-            cin.ignore(256, '\n'); //empty insert puffer
-            cout << "Please insert only full numbers. Please try again." << endl;
-            end = false;
-        } else {
+    cout << "The number is set between 1 and 100, you can write your answer now." << endl;
+    do {
+        input = checkUserInput();
+        if(input > 0){
             end = checkNumber(input, x);
             trys++;
         }
@@ -47,5 +43,20 @@ bool checkNumber(int input, int x) {
         return true;
     } else {
         cout << "An error has occurred, the program will shut down." << endl;
+    }
+}
+
+int checkUserInput(){
+    int input;
+
+    cin >> input;
+    if(cin.fail()){
+        cin.clear(); //reset the error
+        cin.ignore(256, '\n'); //empty insert puffer
+        cout << "Please insert only full numbers. Please try again." << endl;
+        return -1;
+    } else {
+        //end = checkNumber(input, x);
+        return input;
     }
 }
