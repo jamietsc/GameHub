46d7ac8b6a3ddd97f2190c1bf793f1332161eaad
jamietsc <jamie.jentsch2007@gmail.com>
2024-12-22 12:30:27 +0100

    Using a function for checking the number.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <cstdlib>
 
+bool checkNumber(int input, int x);
+
 using namespace std;
 
 int main() {
-    int x = rand() % 100 + 1; //x ist im Bereich von 1 bis 100
-    bool end = false; // Variable um die Schleife zu beenden
-    int input, trys; //Variable um die Eingabe des Nutzer zu speichern
+    int x = rand() % 100 + 1; //x is the random number between 1 and 100
+    bool end;// variable to end the loop
+    int input, trys; //input is for saving the guess of the user. trys for counting the trys the user needed.
 
     cout << "The number is set, you can write your answer now." << endl;
     do{
         cin >> input;
-        if(input < x){
-            cout << "The random number is bigger than your input." << endl;
-            cout << "Please guess again: " << endl;
-        } else if(input > x){
-            cout << "The random number is smaller than your input" << endl;
-            cout << "Please guess again: " << endl;
-        } else if(input == x){
-            cout << "Congratulations, you guessed the right number" << endl;
-            end = true;
-        } else {
-            cout << "An error has occurred, the program will shut down." << endl;
-            return 1;
-        }
+        end = checkNumber(input, x);
     } while (!end);
     return 0;
+}
+
+/**
+ * function to check the number from the user
+ * @param input the input from the user
+ * @param x the number that has to be guess
+ * @return true if correct, false if not correct
+ */
+bool checkNumber(int input, int x) {
+    if(input < x){
+        cout << "The random number is bigger than your input." << endl;
+        cout << "Please guess again: " << endl;
+        return false;
+    } else if(input > x){
+        cout << "The random number is smaller than your input" << endl;
+        cout << "Please guess again: " << endl;
+        return false;
+    } else if(input == x){
+        cout << "Congratulations, you guessed the right number" << endl;
+        return true;
+    } else {
+        cout << "An error has occurred, the program will shut down." << endl;
+        return true;
+    }
 }
